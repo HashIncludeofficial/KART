8e94fdd9ee9df767ce399729ec40274a16910084
Dev Sura <champroxx4596@gmail.com>
2016-12-27 15:08:15 +0530

    FUCK BABy

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/open/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/open/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp64/www/open/catalog/');
+define('DIR_SYSTEM', 'C:/wamp64/www/open/system/');
+define('DIR_IMAGE', 'C:/wamp64/www/open/image/');
+define('DIR_LANGUAGE', 'C:/wamp64/www/open/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp64/www/open/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp64/www/open/system/config/');
+define('DIR_CACHE', 'C:/wamp64/www/open/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp64/www/open/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp64/www/open/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp64/www/open/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp64/www/open/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'data');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
